a7e92c3fe7fec1051f449a22a8864802d0d35349
Own3Dqq <own3dqq@gmail.com>
2023-08-11 00:35:16 +0300

    styled blog footer section

---
+++ js/index.js
@@ -0,0 +1,70 @@
+// /* Slick slider (Hero) */
+// $(document).ready(function () {
+//     $('.hero-slider__list').slick({
+//         arrows: true,
+//         infinite: false,
+//         autoplay: false,
+//         dots: true,
+//         speed: 500,
+//         fade: true,
+//         initialSlide: 0,
+//         cssEase: 'linear',
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         prevArrow: '.hero__prev',
+//         nextArrow: '.hero__next',
+//         dotsClass: 'hero__wrapper',
+//         appendDots: '.hero__switchers',
+//         slickPrev: '.hero__prev',
+//         slickPrev: '.hero__next',
+//     });
+// });
+
+/* Works Slider */
+// $('.work-slider').slick({
+//     dots: true,
+//     infinite: false,
+//     speed: 300,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 3,
+//                 infinite: true,
+//                 dots: true,
+//             },
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//             },
+//         },
+//         {
+//             breakpoint: 320,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//             },
+//         },
+//     ],
+// });
+
+// /* Blog Slider */
+// $(document).ready(function () {
+//     $('.blog__list').slick({
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         arrows: false,
+//         infinite: false,
+//         dots: true,
+//         dotsClass: 'blog__dots',
+//         appendDots: '.blog__wrapper',
+//     });
+// });
+
+/* Hamburger Menu */
