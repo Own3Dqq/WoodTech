3cbbc7b1158b56e7b700444ef95f24026f074bc1
Own3Dqq <own3dqq@gmail.com>
2023-08-12 19:59:30 +0300

    fixed style on footer section

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './index.html';
 import './js/index';
 import 'animate.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './js/assets/fontawesome/fontawesome-all.min';
 
 /* Style */
-import 'bootstrap';
 import './index.scss';
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,17 @@
-// core version + navigation, pagination modules:
 import Swiper from 'swiper';
 import { Navigation, Pagination, Scrollbar, EffectFade } from 'swiper/modules';
-// import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-const swiper = new Swiper('.hero__swiper', {
+const heroSwiper = new Swiper('.hero__swiper', {
     modules: [Navigation, Pagination, Scrollbar, EffectFade],
     speed: 300,
-    // effect: '',
-    // fadeEffect: {
-    //     crossFade: true,
-    // },
-    slidesPerView: 1,
+    spaceBetween: 40,
+    effect: 'flip',
+    cssMode: true,
 
-    // If we need pagination
+    // Pagination
     pagination: {
         el: '.control__numbers',
         type: 'custom',
@@ -24,10 +20,14 @@ const swiper = new Swiper('.hero__swiper', {
         renderCustom: function (swiper, current, total) {
             return (
                 '<span class="counter">' +
+                '0' +
+                '' +
                 current +
                 '</span>' +
                 '<span class="devide">|</span> ' +
                 '<span class="total">' +
+                '0' +
+                '' +
                 total +
                 '</span>'
             );
@@ -48,74 +48,3 @@ const swiper = new Swiper('.hero__swiper', {
         hide: false,
     },
 });
-
-// /* Slick slider (Hero) */
-// $(document).ready(function () {
-//     $('.hero-slider__list').slick({
-//         arrows: true,
-//         infinite: false,
-//         autoplay: false,
-//         dots: true,
-//         speed: 500,
-//         fade: true,
-//         initialSlide: 0,
-//         cssEase: 'linear',
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         prevArrow: '.hero__prev',
-//         nextArrow: '.hero__next',
-//         dotsClass: 'hero__wrapper',
-//         appendDots: '.hero__switchers',
-//         slickPrev: '.hero__prev',
-//         slickPrev: '.hero__next',
-//     });
-// });
-
-/* Works Slider */
-// $('.work-slider').slick({
-//     dots: true,
-//     infinite: false,
-//     speed: 300,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     responsive: [
-//         {
-//             breakpoint: 1024,
-//             settings: {
-//                 slidesToShow: 3,
-//                 slidesToScroll: 3,
-//                 infinite: true,
-//                 dots: true,
-//             },
-//         },
-//         {
-//             breakpoint: 600,
-//             settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1,
-//             },
-//         },
-//         {
-//             breakpoint: 320,
-//             settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1,
-//             },
-//         },
-//     ],
-// });
-
-// /* Blog Slider */
-// $(document).ready(function () {
-//     $('.blog__list').slick({
-//         slidesToShow: 3,
-//         slidesToScroll: 3,
-//         arrows: false,
-//         infinite: false,
-//         dots: true,
-//         dotsClass: 'blog__dots',
-//         appendDots: '.blog__wrapper',
-//     });
-// });
-
-/* Hamburger Menu */
